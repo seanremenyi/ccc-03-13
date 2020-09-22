b77ea7d635ab83894fcc091f76ba2ed63662d722
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-09-22 10:05:05 +1000

    lesson start

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://app:Testing1@localhost:5432/library_api"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
     return db  
--- schemas/BookSchema.py
+++ schemas/BookSchema.py
@@ -1,9 +1,12 @@
 from main import ma
 from models.Book import Book
+from marshmallow.validate import Length
 
 class BookSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Book
 
+    title = ma.String(required=True, validate=Length(min=1))
+    
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
